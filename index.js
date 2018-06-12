2d2327c7286e7f2ce81d6b5ae4e5fd9e9e2d6337
Jeff <jeffreyrkang@gmail.com>
2018-06-12 12:02:44 -0700

    Add command line flags, implement redeployment

--- index.js
+++ index.js
@@ -2,12 +2,38 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const commandLineArgs = require('command-line-args');
 const inquirer = require('./lib/inquirer');
-const commands = require('./lib/commands');
 
-clear();
-console.log(chalk.blue(figlet.textSync('PWA Creator', { horizontalLayout: 'full' })));
-const run = async () => {
-  await inquirer.generateTemplate();
-};
-run();
+const optionDefinitions = [
+  { name: 'help', alias: 'h', type: Boolean },
+  { name: 'redeploy', alias: 'r', type: Boolean },
+];
+const options = commandLineArgs(optionDefinitions);
+
+// Help flag entered, print help text
+if (options.help) {
+  clear();
+  console.log(chalk.blue(figlet.textSync('PWA Creator', { horizontalLayout: 'full' })));
+  console.log('Welcome to create-pwa! This is the help prompt. 🔥 🔥 🔥\n');
+}
+// Redeploy flag entered, initiate redeployment
+else if (options.redeploy) {
+  clear();
+  console.log(chalk.blue(figlet.textSync('PWA Creator', { horizontalLayout: 'full' })));
+  console.log('Welcome to create-pwa! Launching redeployment prompt. 🔥 🔥 🔥\n');
+  const run = async () => {
+    await inquirer.redeploy();
+  };
+  run();
+}
+// No options, go to standard prompt
+else {
+  clear();
+  console.log(chalk.blue(figlet.textSync('PWA Creator', { horizontalLayout: 'full' })));
+  console.log('Welcome to create-pwa! Launching code generator and deployment prompt. 🔥 🔥 🔥\n');
+  const run = async () => {
+    await inquirer.generateTemplate();
+  };
+  run();
+}
