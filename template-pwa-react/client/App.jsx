7133966758a0eb3267e65a6ff27b920ef44f6964
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-06-19 20:23:47 -0700

    troubleshooting aws appName

---
+++ template-pwa-react/client/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app-container">
+        <h1>the Forge</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
