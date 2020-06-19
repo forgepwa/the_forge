1153234d4f87e4b51759f9de4f128edea96026a0
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2020-06-18 23:28:30 -0700

    adds sw

--- lib/templates/apollo-pwa/client/components/Main.jsx
+++ lib/templates/apollo-pwa/client/components/Main.jsx
@@ -37,3 +37,17 @@ export default function Main() {
 
   return <ToDoList createTodo={createTodo} removeTodo={removeTodo} />;
 }
+
+// Check that service workers are supported
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
